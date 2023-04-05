deeca050e73d64ecf08a14493698c6ca02cbfffc
Alexis Rodriguez <avrodzz@gmail.com>
2023-04-04 21:46:01 -0700

    Finished up my version of the Magic 8 Ball app using what I learned from Dicee.

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -9,10 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var ballImageView: UIImageView!
     
-    let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
-
-
-
+    let ballArray = [UIImage(named: "ball1"), UIImage(named: "ball2"), UIImage(named: "ball3"), UIImage(named: "ball4"), UIImage(named: "ball5")]
+    
+    let initialBallIndex = 2
+    
+    override func viewDidLoad() {
+        // Default the ball image to ball3
+        ballImageView.image = ballArray[initialBallIndex]
+    }
+    
+    @IBAction func askButtonPressed(_ sender: UIButton) {
+        // If the ask button is pressed, provide a new ball image answer
+        ballImageView.image = ballArray[Int.random(in: 0...ballArray.count - 1)]
+    }
 }
 
